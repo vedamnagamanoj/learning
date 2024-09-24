546b0db808f5c493eed2f6c1d1153bba52f5983d
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-09-25 03:37:41 +0530

    end of day 5

--- day_5.js
+++ day_5.js
@@ -18,28 +18,66 @@
 
 // createBooking("LH123", undefined, 100);
 
-const flights = "LH234";
-const jonas = {
-  name: "Jonas Schmedtmann",
-  passport: 23456778899,
-};
+// const flights = "LH234";
+// const jonas = {
+//   name: "Jonas Schmedtmann",
+//   passport: 23456778899,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = "LH999";
-  passenger.name = "Mr. " + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LH999";
+//   passenger.name = "Mr. " + passenger.name;
 
-  if (passenger.passport === 23456778899) console.log("Check In");
-  else console.log("Wrong Passport");
-};
+//   if (passenger.passport === 23456778899) console.log("Check In");
+//   else console.log("Wrong Passport");
+// };
 
-checkIn(flights, jonas);
-console.log(flights, jonas);
+// checkIn(flights, jonas);
+// console.log(flights, jonas);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000000);
-};
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
 
-console.log(jonas);
-newPassport(jonas);
+// console.log(jonas);
+// newPassport(jonas);
 
-console.log(jonas);
+// console.log(jonas);
+
+// console.log(newPassport.toString());
+
+// const oneWord = function (str) {
+//   return str.replaceAll(" ", "").toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(" ");
+
+//   return [first.toUpperCase(), ...others].join(" ");
+// };
+
+// const transformer = function (str, fn) {
+//   return fn(str);
+// };
+
+// console.log(transformer("JavaScript is the best!", oneWord));
+// console.log(transformer("JavaScript is the best!", upperFirstWord));
+
+// function high5(item) {
+//   console.log(`Hi! ${item}`);
+// }
+
+// ["Jonas", "Martin", "Adam"].forEach((item) => high5(item));
+
+function greet(greeting) {
+  console.log(this);
+  return (name) => {
+    console.log(`${greeting} ${name} ${this}`);
+  };
+}
+
+greet("Hello")("Jonas");
+
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+greetArr("Hello")("Jonas");
