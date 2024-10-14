d32e2815b1a09fa214d588b969a0f0964681f237
Vedam Naga Manoj <vedamnagamanoj@gmail.com>
2024-10-14 19:43:15 +0530

    end of day 12

--- mini_project_1_v2/script.js
+++ mini_project_1_v2/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (evnt) {
+  evnt.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,26 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Test Area
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+console.log(document.querySelectorAll('.section'));
+console.log(document.getElementsByTagName('button'));
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+
+// .insertAdjacentHTML()
+const cookieMessage = document.createElement('div');
+cookieMessage.classList.add('cookie-message');
+// cookieMessage.textContent = `We use cookie for improved functionality and analytics`;
+cookieMessage.innerHTML = `We use cookie for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>`;
+
+header.prepend(cookieMessage);
+header.append(cookieMessage);
+
+console.log();
